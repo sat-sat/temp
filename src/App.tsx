64f6be83aa9b14586a1155c18f21ed522aad304f
Jason Pang <jason@lassolabs.xyz>
2024-09-01 12:13:04 -0700

    Change default color scheme to dark

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} defaultColorScheme="dark">
       <QueryClientProvider client={queryClient}>
         <Router>
           <Routes>
