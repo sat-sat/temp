0fc5ef401526221106d8d25e699902d6e9433756
Jason Pang <jason@lassolabs.xyz>
2024-09-01 13:30:26 -0700

    Add nav layout with search action

--- src/NavLayout.tsx
+++ src/NavLayout.tsx
@@ -1,10 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { Tabs } from "@mantine/core";
+import { useCallback } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import styles from "./NavLayout.module.css";
+import SearchInput from "./SearchInput";
+import Logo from "./logo.svg?react";
 
 export default function NavLayout() {
+  const navigate = useNavigate();
+
+  const onSearch = useCallback((value: string) => {
+    navigate("/movies", { state: { search: value } });
+  }, []);
+
   return (
-    <div>
-      Nav Layout
+    <main>
+      <nav>
+        <Logo />
+        <div className={styles.navContentRight}>
+          <Tabs
+            color="white"
+            variant="pills"
+            defaultValue="movies"
+            classNames={styles}
+          >
+            <Tabs.List defaultValue="movies">
+              <Tabs.Tab color="blue" value="movies">
+                Movies
+              </Tabs.Tab>
+              <Tabs.Tab value="favorites">Favorites</Tabs.Tab>
+            </Tabs.List>
+          </Tabs>
+          <SearchInput onSearch={onSearch} />
+        </div>
+      </nav>
       <Outlet />
-    </div>
+    </main>
   );
 }
